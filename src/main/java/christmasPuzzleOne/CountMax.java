d88c26330cd7fb9459f9c7d0fba0a502a69746b4
Vikram Suriyanarayanan <vikramsuriyanarayanan@Vikrams-MacBook-Pro.local>
2021-12-02 12:11:40 -0600

    Initial commit

---
+++ src/main/java/christmasPuzzleOne/CountMax.java
@@ -0,0 +1,56 @@
+package christmasPuzzleOne;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class CountMax {
+
+	public static void main(String[] args) throws FileNotFoundException {
+		// TODO Auto-generated method stub
+		List<Integer> inputArray = new ArrayList<Integer>();
+	    Scanner inFile1 = new Scanner(new File("inputFile"));
+	    
+		while (inFile1.hasNext()) {
+		    inputArray.add(Integer.valueOf(inFile1.nextLine()));
+		}
+		
+		CountMax c = new CountMax();
+		System.out.println(c.getMax(inputArray));
+		System.out.println("*******************");
+		
+		int[] arr = inputArray.stream().mapToInt(i -> i).toArray();
+		
+		System.out.println(c.getSlidingWindow(arr));
+
+	}
+	
+	public int getMax(List<Integer> input) {
+		int output = 0 ;
+		
+		for(int i=1;i<input.size();i++) {
+			if (input.get(i) > input.get(i-1))
+				output++;
+		}
+		
+		return output;
+	}
+	
+	public int getSlidingWindow(int[] input) {
+		int output=0;
+		int initialSum = input[0]+input[1]+input[2];
+		
+		for(int i=3;i<input.length;i++) {
+			int currentSum = initialSum - input[i-3] + input[i];
+			
+			if(currentSum > initialSum) 
+				output++;
+			
+			initialSum = currentSum;
+		}
+		return output;
+	}
+
+}
---
+++ src/main/java/christmasPuzzleTwo/PilotFinding.java
@@ -0,0 +1,75 @@
+package christmasPuzzleTwo;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import christmasPuzzleOne.CountMax;
+
+public class PilotFinding {
+	
+	
+	public static final String HORIZONDAL_ADD = "forward";
+	public static final String VERTICAL_ADD = "down";
+	public static final String VERTICAL_SUB = "up";
+
+
+	public static void main(String[] args) throws FileNotFoundException {
+		// TODO Auto-generated method stub
+		List<String> inputArray = new ArrayList<String>();
+	    Scanner inFile1 = new Scanner(new File("inputFile2"));
+	    
+		while (inFile1.hasNext()) {
+		    inputArray.add(inFile1.nextLine());
+		}
+		
+		PilotFinding c = new PilotFinding();
+		System.out.println(c.getDirection(inputArray));
+		System.out.println("*******************");
+		
+		System.out.println(c.getAimedDirection(inputArray));
+		System.out.println("*******************");
+	}
+	
+	public int getDirection(List<String> input) {
+		
+		int horizondal = 0;
+		int vertical = 0;
+		
+		for(String in:input) {
+			String[] inSplit = in.split(" ");
+			if(inSplit[0].equalsIgnoreCase(HORIZONDAL_ADD)) {
+				horizondal+=Integer.parseInt(inSplit[1]);
+			} else if (inSplit[0].equalsIgnoreCase(VERTICAL_ADD)) {
+				vertical +=Integer.parseInt(inSplit[1]);
+			} else {
+				vertical -=Integer.parseInt(inSplit[1]);
+			}
+			
+		}
+		return horizondal*vertical;
+	}
+	
+	public int getAimedDirection(List<String> input) {
+		int horizondal = 0;
+		int depth = 0;
+		int aim = 0;
+		
+		for(String in:input) {
+			String[] inSplit = in.split(" ");
+			if(inSplit[0].equalsIgnoreCase(HORIZONDAL_ADD)) {
+				depth += aim*Integer.parseInt(inSplit[1]);
+				horizondal+=Integer.parseInt(inSplit[1]);
+			} else if (inSplit[0].equalsIgnoreCase(VERTICAL_ADD)) {
+				aim +=Integer.parseInt(inSplit[1]);
+			} else {
+				aim -=Integer.parseInt(inSplit[1]);
+			}
+			
+		}
+		return horizondal*depth;
+	}
+
+}
